8680229a9e440fc93dfc348854e1faadafe31f06
Mcamilabfh <89530877+Mcamilabfh@users.noreply.github.com>
2021-08-30 16:12:02 -0400

    Adicionado documentacao

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Teste-Camila---ENG
+
+## Pré requisitos:
+ 
+  - Dockligth
+  - ESP-IDF
+
+Instalação ESP-IDF (espressif)[https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/]
+
+## GPIO
+
+|    FUN    |    GPIO     |
+|-----------|-------------|
+|  TXD_PIN  |  GPIO_NUM_1 |
+|  RXD_PIN  |  GPIO_NUM_3 |
+|  GPIO_LED |  GPIO_NUM_2 |
+
+## Wifi
+
+|       FUN       |      GPIO       |
+|-----------------|-----------------|
+|  ESP_WIFI_SSID  |  ENG TECNOLOGIA |
+|  ESP_WIFI_PASS  |     eng12345    |
+
+## Protocolo **Sem** parâmetros
+
+|  EN  |  CMD  | N PARAMETRO | CHECKSUM |  DESCRIÇÃO  |
+|------|-------|-------------|----------|-------------|
+| 0x01 | 0x00  |     0x00    |   0x01   |  Ligar led  |
+| 0x01 | 0x01  |     0x00    |   0x02   | desligar led|
+| 0x01 | 0x02  |     0x00    |   0x03   |  toggle led |
+| 0x01 | 0x03  |     0x00    |   0x04   |  loopback   |
+
+## Protocolo **Com** parâmetros
+
+|  EN  |  CMD  | N PARAMETRO |PARAMETRO 1| PARAMETRO 2 | CHECKSUM |  DESCRIÇÃO  |
+|------|-------|-------------|-----------|-------------|----------|-------------|
+| 0x01 | 0x03  |     0x02    |    0x01   |     0x03    |   0x0A   |   loopback  |
+
+Executando projeto:
+
+'''
+    Abra o terminal do idf e execute os comandos relativos a sua máquina:
+
+    cd ~/TESTE-CAMILA---ENG
+    idf.py build
+    idf.py -p COM3 flash
+
+'''
--- main/main.c
+++ main/main.c
@@ -3,36 +3,44 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_log.h"
+#include "esp_wifi.h"
 #include "driver/uart.h"
 #include "driver/gpio.h"
-#include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
 
-static const int BUF_SIZE = 1024;
+static const int BUF_SIZE = 1024;   ///< Buffer para Rx da UART Serial.
 
+/**
+ *  Estrutura para comunicação na fila responsável por enviar os bytes para o processo do tx_task.
+ *  Usado como item da fila para comunicação com o tx_task.
+ **/
 typedef struct {
-    uint8_t* data;
-    int size;
+    uint8_t* data;  ///< Buffer de bytes a serem enviados pela Serial.
+    int size;       ///< Tamanho em bytes do espaço ocupado pelos bytes no buffer data.
 } send_event_t;
 
-#define TXD_PIN (GPIO_NUM_1)
-#define RXD_PIN (GPIO_NUM_3)
-#define GPIO_LED (GPIO_NUM_2)
-#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_LED) 
 
-#define ESP_WIFI_SSID      "ENG TECNOLOGIA"
-#define ESP_WIFI_PASS      "eng12345"
-#define ESP_WIFI_CHANNEL   10
-#define MAX_STA_CONN       100
+#define TXD_PIN (GPIO_NUM_1)                    ///< Pino que será usado para tx da UART.
+#define RXD_PIN (GPIO_NUM_3)                    ///< Pino que será usado para rx da UART.
+#define GPIO_LED (GPIO_NUM_2)                   ///< Pino que será usado para ligar ou desligar o led.
+#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_LED)    ///< Máscara de bits para configuração do pino do led.
 
-QueueHandle_t xQueue;
-QueueHandle_t xQueueLed;
+#define ESP_WIFI_SSID      "ENG TECNOLOGIA" ///< Definição do nome do ponto de acesso da wifi.
+#define ESP_WIFI_PASS      "eng12345"       ///< Definição da senha do ponto de acesso da wifi.
+#define ESP_WIFI_CHANNEL   10               ///< Definição do canal do ponto de acesso da wifi.
+#define MAX_STA_CONN       100              ///< Definição da quantidade limite de usuários permitidos na conexão do ponto de acesso da wifi.
 
-static const char *TAG = "wifi ENG";
+QueueHandle_t xQueue;    ///> Fila para comunicação da taks com a tarefa de transmissão.
+QueueHandle_t xQueueLed; ///> Fila para comunicação da taks do led com a tarefa de transmissão do led.
 
+static const char *TAG = "wifi ENG"; ///> Tag para indicação dos logs.
+
+/**
+ *  Indicação de usuários conectados e desconectados ao ponto de acesso da wifi.
+ **/
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
@@ -47,7 +55,7 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 /**
- * 
+ * Configurações gerais do wifi e ponto de acesso.
  **/
 void wifi_init(void)
 {
@@ -82,9 +90,11 @@ void wifi_init(void)
     ESP_LOGI(TAG, "wifi_init finished. SSID:%s password:%s channel:%d",
              ESP_WIFI_SSID, ESP_WIFI_PASS, ESP_WIFI_CHANNEL);
 }
-
+/**
+ * Inicialização do wifi e uso do nvs para salvar informações básicas do wifi na memória flash caso o esp32 seja desernegizado.
+ **/
 void init_nvs(void) {
-    //Initialize Non Vollatile Storage
+    
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
       ESP_ERROR_CHECK(nvs_flash_erase());
@@ -93,53 +103,58 @@ void init_nvs(void) {
     ESP_ERROR_CHECK(ret);
 }
 
+/**
+ * Inicializa e configura a UART.
+ **/
 void init_uart(void) {
     const uart_config_t uart_config = {
-            .baud_rate = 115200,
-            .data_bits = UART_DATA_8_BITS,
-            .parity = UART_PARITY_DISABLE,
-            .stop_bits = UART_STOP_BITS_1,
-            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-            .source_clk = UART_SCLK_APB,
+            .baud_rate = 115200,                   ///< Taxa de transmissão de dados na velocidade do esp 115200.
+            .data_bits = UART_DATA_8_BITS,         ///< Número de bits de dados = 8.               
+            .parity = UART_PARITY_DISABLE,         ///< Bit de paridade desabilitado.
+            .stop_bits = UART_STOP_BITS_1,         ///< Stop bit configurado com 1 bit.
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE, ///< Controle de fluxo desabilitado.
+            .source_clk = UART_SCLK_APB,           ///< Clock para o cálculo do baude rate.                        
     };
-    // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUM_1, 2048, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+   
+    uart_driver_install(UART_NUM_1, 2048, 0, 0, NULL, 0);                               ///< Instalação do driver da UART.
+    uart_param_config(UART_NUM_1, &uart_config);                                        ///< Configuranção da UART.
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); ///< Set dos pinos tx e rx da UART.
 }
-
+/**
+ * Configura o GPIO do led.
+ **/
 void init_led(void)
 {
-    gpio_config_t io_conf;
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set,e.g.GPIO18/19
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
+    gpio_config_t io_conf;                 
+    io_conf.intr_type = GPIO_INTR_DISABLE;      ///< Disabilita interrupção.
+    io_conf.mode = GPIO_MODE_OUTPUT;            ///< set as saídas mode.
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL; ///< Máscara de bit dos pinos que serão setadas.
+    io_conf.pull_down_en = 0;                   ///< Desabilita o modo pull-down.                
+    io_conf.pull_up_en = 0;                     ///< Desabilita o modo pull-up.       
+    gpio_config(&io_conf);                      ///< Configura o GPIO.       
 }
 
-void sendData(uint8_t* data, int size)
+void sendData(uint8_t* data, int size) ///< SendData envia para a fila a informação que será transmitida para a serial.
 {
-    send_event_t event = {
-            .data = data,
-            .size = size
+    send_event_t event = { ///< Struct event envia dados mutuamente:
+            .data = data,  ///< Vetor de informações.
+            .size = size   ///< Tamanho das informações
     };
 
-    xQueueSend(xQueue, (void*) &event, (TickType_t) 100);
+    /**
+     * Tempo de espera para a função alocar o item na fila evitando o deadlock.
+     **/
+
+    xQueueSend(xQueue, (void*) &event, (TickType_t) 100);   
 }
 
 void sendLed(uint8_t data)
 {
-    xQueueSend(xQueueLed, (void*) &data, (TickType_t) 100);
+    xQueueSend(xQueueLed, (void*) &data, (TickType_t) 100); 
 }
-
+/**
+ * Cálculo de checksum, a soma de todas as posições com excessão do próprio checksum.
+ **/
 uint8_t checksum(uint8_t* buff, size_t len)
 {
     uint8_t sum;
@@ -147,13 +162,15 @@ uint8_t checksum(uint8_t* buff, size_t len)
         sum += *(buff++);
     return (uint8_t)sum;
 }
-
+/**
+ * Task para o led
+ **/
 static void led_task(void *arg)
 {
-    uint8_t data = 0;
-    uint8_t led_state = 0;
+    uint8_t data = 0;      ///< Variavél que recebe os itens da fila.
+    uint8_t led_state = 0; ///< Estado do led.
 
-    gpio_set_level(GPIO_LED, led_state);
+    gpio_set_level(GPIO_LED, led_state); 
 
     while (1) {
         if(xQueueReceive(xQueueLed, &data, ( TickType_t ) 100 ) == pdPASS)
@@ -169,7 +186,10 @@ static void led_task(void *arg)
         }
     }
 }
-
+/**
+ * Processo de transmissão, também apresenta tempo de espera na fila afim de evitar o deadlock. 
+ * Porteriormente, irá passar a informação a ser enviada e o tamanho da informação.
+ **/
 static void tx_task(void *arg)
 {
     send_event_t event = {0};
@@ -181,7 +201,11 @@ static void tx_task(void *arg)
         }
     }
 }
-
+/**
+ * Aloca um buffer para receber informações, quando a informação é recebida ela é armazenada dentro do buffer data.
+ * Quando o pacote é recebido a quantidade de bytes usado é informada.
+ * Se o número de bytes for maior do que 0, inicia-se o protocolo
+ **/
 static void rx_task(void *arg)
 {
     uint8_t* data = (uint8_t*) malloc(BUF_SIZE+1);
@@ -190,49 +214,50 @@ static void rx_task(void *arg)
         const int rxBytes = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 1000 / portTICK_RATE_MS);
         if (rxBytes > 0) {
 
-            uint8_t start_byte = data[0];
-            uint8_t cmd_byte = data[1];
-            uint8_t param_byte = data[2];
-            uint8_t checksum_byte = 0;
-            uint8_t* params;
+            uint8_t start_byte = data[0]; ///< Inicio do byte.
+            uint8_t cmd_byte = data[1];   ///< Byte de comando.
+            uint8_t param_byte = data[2]; ///< Número de parâmentros.
+            uint8_t checksum_byte = 0;    ///< Checksum.
+            uint8_t* params;              ///< Vetor de parâmetro.
 
-            int i = 0;
+            int i = 0;                    
 
-            uint8_t *resp = malloc(sizeof(uint8_t));
+            uint8_t *resp = malloc(sizeof(uint8_t)); ///< Buffer de resposta padrão ACK.
             resp[0] = 6;
 
-            if (start_byte == 1) {
-                if (param_byte > 0) {
-                    // cria um vetor de 1 byte vezes a quantidade de parâmetros, cada parâmetro ocupa 1 byte
-                    params = (uint8_t*) malloc(sizeof(uint8_t) * param_byte);
 
-                    for(i = 0; i < param_byte; i++) {
-                        params[i] = data[3 + i];
+            if (start_byte == 1) {    ///< Se o start byte é igual a 1 o protocolo é iniciado.
+                if (param_byte > 0) { ///< Se o parâmetro é maior que 0 então: 
+            
+                    params = (uint8_t*) malloc(sizeof(uint8_t) * param_byte); ///> Aloca um vetor de 1 byte vezes a quantidade de parâmetros, cada parâmetro ocupa 1 byte
+
+                    for(i = 0; i < param_byte; i++) { ///< For para detecção de parâmetros
+                        params[i] = data[3 + i];      
                     }
 
                     checksum_byte = data[3 + i];
                 } else {
                     checksum_byte = data[3];
                 }
-
+                /**
+                 * Faz a comparação entre o checksum byte e o checksum para verificar se o pacote foi recebido de forma integra (Sem perdas).
+                 **/
                 if (checksum_byte == checksum(data, sizeof(uint8_t) * (3 + i))) {
                     switch (cmd_byte) {
-                        case 0x00:
-                            // comando ligar led
-                            sendLed(0x01);
-                            sendData(resp, sizeof(uint8_t));
-                            break;
-                        case 0x01:
-                            // comando desligar led
+                    
+                        case 0x00:///< comando desligar led
                             sendLed(0x00);
                             sendData(resp, sizeof(uint8_t));
                             break;
-                        case 0x02:
-                            // comando toggle led
+                            case 0x01:///< comando ligar led
+                            sendLed(0x01);
+                            sendData(resp, sizeof(uint8_t));
+                            break;
+                        case 0x02:///< comando toggle led
                             sendLed(0x02);
                             sendData(resp, sizeof(uint8_t));
                             break;
-                        case 0x03:
+                        case 0x03:///< comando loopback
                             sendData(data, rxBytes);
                             break;
                     }
@@ -245,7 +270,9 @@ static void rx_task(void *arg)
 }
 
 /**
- * 
+ * Funções tasks criadas para rodarem simultâneamente.
+ * Função principal do programa cria as filas para comunicação das funções tx task e led task, chama funções de inicialização para o GPIO do led
+ * inicializar e configurar a UART, inicializao o NVS e inicializa o wifi.
  **/
 void app_main(void)
 {
